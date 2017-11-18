40f900972031460921abad2e5605d2ab6abe1a32
Avinash <@purdue.edu>
2017-11-18 00:26:25 -0500

    first commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Avinash Singh on 17/11/17.
+//  Copyright © 2017 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
